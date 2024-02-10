f2fe21e285de132bbab99a640b4747513c224095
Om Parmar <omparmar10@gmail.com>
2024-02-10 18:20:48 -0500

    added i chart /usr/local/bin/python3 /Users/omparmar/Documents/Capability-Sixpack-Normal/i_chart.py

--- i_chart.py
+++ i_chart.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def i_chart(filename, sheet_name, cl_index):
+def i_chart(filename, sheet_name, col_index, sub_group=1):
     """
     Generates an I chart with out-of-control points highlighted in red. Adds a margin for LSL and USL 
     that is always 30% above and below the maximum and minimum values on the Y-axis.
@@ -28,12 +28,14 @@ def i_chart(filename, sheet_name, cl_index):
     Returns:
         None.
     """
+    if(sub_group!=1):
+        raise ValueError("Subgroup Size has to be 1 for IChart Analysis")
 
     # Read data from the XLSX file
     df = pd.read_excel(filename, sheet_name=sheet_name)
 
     # Select data based on CL index
-    data = df.iloc[:, cl_index].tolist()
+    data = df.iloc[:, col_index].tolist()
 
     # Calculate mean and standard deviation
     mean = np.mean(data)
@@ -67,7 +69,7 @@ def i_chart(filename, sheet_name, cl_index):
 
     # Set labels and title
     plt.xlabel('#Observation')
-    plt.ylabel(df.columns[cl_index])
+    plt.ylabel(df.columns[col_index])
     plt.title('I Chart with Out-of-Control Points Highlighted and Margin for LSL and USL')
 
     # Add legend
@@ -79,6 +81,5 @@ def i_chart(filename, sheet_name, cl_index):
 # Example usage:
 filename = "Data.xlsx"  # Replace with your actual file path
 sheet_name = "Sheet1"  # Replace with the correct sheet name if needed
-cl_index = 3  # Adjust the CL index based on your data structure
+cl_index = 4  # Adjust the CL index based on your data structure
 i_chart(filename, sheet_name, cl_index)
-    
